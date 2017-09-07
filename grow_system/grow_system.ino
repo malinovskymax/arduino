6a4bc5498f15592121c708d9da7a050a1ae75137
Maxim Malinovsky <malinovsky.dev@gmail.com>
2017-09-08 00:19:42 +0300

    Update grow_system.ino
    v 0.3

--- grow_system/grow_system.ino
+++ grow_system/grow_system.ino
@@ -1,13 +1,15 @@
 #define LIGHT_PIN 2
-#define START_PHASE "day"
 
-long start_offset_s = 0L; // will be applied to start phase on first cycle
+ String start_phase = "night"; // night or day
+ String phase = "";
+
+long start_offset_s =   0L; // on the first cycle start phase will be reduced for the given ammount of seconds
 
 long basic_night_duration_s = 36000L; // 0 to 36000 s
 long basic_day_duration_s   = 50400L; // 0 to 50400 s
 
-int night_duration_correction_s = 0; // 343
-int day_duration_correction_s   = 0; // 480
+int night_duration_correction_s = 1500;
+int day_duration_correction_s   = 2100;
 
 long night_duration_s = basic_night_duration_s - night_duration_correction_s;
 long day_duration_s   = basic_day_duration_s   - day_duration_correction_s;
@@ -15,11 +17,14 @@ long day_duration_s   = basic_day_duration_s   - day_duration_correction_s;
 long day   = 0L;
 long night = 0L;
 
+bool phase_changed = true;
+
 void setup() {
-  //Serial.begin(9600);
   pinMode(LIGHT_PIN, OUTPUT);
 
-  if (START_PHASE == "day") {
+  phase = start_phase;
+
+  if (start_phase == "day") {
     day = day_duration_s - start_offset_s;
   } else {
     night = night_duration_s - start_offset_s;
@@ -30,15 +35,32 @@ void loop() {
   // tick
   delay(1000);
 
-  if(night < night_duration_s) {                 // it's day, turn light on
-    digitalWrite(LIGHT_PIN, LOW);
-    if(day == 0) {                 // it's the end of the twenty-four hours cycle
-      day   = day_duration_s;
+  if (phase == "day") {
+    if (day > 0) {
+      --day;
+    } else {
       night = night_duration_s;
+      day = day_duration_s;
+      phase = "night";
+      phase_changed = true;
     }
-    --day;
-  } else {                         // it's night, turn light off
-    digitalWrite(LIGHT_PIN, HIGH);
-    --night;
+  } else { // night
+    if (night > 0) {
+      --night;
+    } else {
+      night = night_duration_s;
+      day = day_duration_s;
+      phase = "day";
+      phase_changed = true;
+    }
+  }
+
+  if (phase_changed) {
+    phase_changed = false;
+    if (phase == "day") {
+      digitalWrite(LIGHT_PIN, LOW);
+    } else {
+      digitalWrite(LIGHT_PIN, HIGH);
+    }
   }
 }
