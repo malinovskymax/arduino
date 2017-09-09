664d9f656b3b423c5a8de55667be1435e2b7bc75
Maxim Malinovsky <malinovsky.dev@gmail.com>
2017-09-09 16:17:38 +0300

    Update grow_system.ino
    v 0.3.1

--- grow_system/grow_system.ino
+++ grow_system/grow_system.ino
@@ -1,15 +1,15 @@
 #define LIGHT_PIN 2
 
- String start_phase = "night"; // night or day
+ String start_phase = "day"; // night or day
  String phase = "";
 
-long start_offset_s =   0L; // on the first cycle start phase will be reduced for the given ammount of seconds
+long start_phase_duration =   17100L; // on the first cycle start phase will be reduced for the given ammount of seconds
 
 long basic_night_duration_s = 36000L; // 0 to 36000 s
 long basic_day_duration_s   = 50400L; // 0 to 50400 s
 
-int night_duration_correction_s = 1500;
-int day_duration_correction_s   = 2100;
+int night_duration_correction_s = 75; //75 s
+int day_duration_correction_s   = 53; //53 s
 
 long night_duration_s = basic_night_duration_s - night_duration_correction_s;
 long day_duration_s   = basic_day_duration_s   - day_duration_correction_s;
@@ -20,24 +20,26 @@ long night = 0L;
 bool phase_changed = true;
 
 void setup() {
+  Serial.begin(9600);
   pinMode(LIGHT_PIN, OUTPUT);
 
   phase = start_phase;
 
   if (start_phase == "day") {
-    day = day_duration_s - start_offset_s;
+    day = start_phase_duration;
   } else {
-    night = night_duration_s - start_offset_s;
+    night = start_phase_duration;
   }
 }
 
 void loop() {
-  // tick
-  delay(1000);
+  Serial.print(phase);
+  Serial.print(" seconds left: ");
 
   if (phase == "day") {
     if (day > 0) {
-      --day;
+      Serial.println(day);
+      day -= 2;
     } else {
       night = night_duration_s;
       day = day_duration_s;
@@ -46,7 +48,8 @@ void loop() {
     }
   } else { // night
     if (night > 0) {
-      --night;
+      Serial.println(night);
+      night -= 2;
     } else {
       night = night_duration_s;
       day = day_duration_s;
@@ -56,6 +59,7 @@ void loop() {
   }
 
   if (phase_changed) {
+    Serial.println("PHASE_CHANGED");
     phase_changed = false;
     if (phase == "day") {
       digitalWrite(LIGHT_PIN, LOW);
@@ -63,4 +67,7 @@ void loop() {
       digitalWrite(LIGHT_PIN, HIGH);
     }
   }
+  Serial.println("--------------------");
+    // tick
+  delay(1997);
 }
